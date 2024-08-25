d28aff9c21c88c6f7218d8398bac694c41140249
Md-Ali-Siddiki <159272300+Md-Ali-Siddiki@users.noreply.github.com>
2024-08-25 09:52:24 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-  module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {
-        gridTemplateColumns: {
-          '12-custom': 'repeat(12, minmax(0, 1fr))',
-        },
-        gridColumn: {
-          'span-2.5': 'span 2.5 / span 2.5',
-        },
-      },
-    },
-  };
-  
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
